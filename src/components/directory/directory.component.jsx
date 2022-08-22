0a419463bb5b1a3618719f67090a0fad3a98039f
An Dyk Bui <andyk.bui@plus4u.net>
2022-08-22 22:28:32 +0300

    add navigation bar

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from "react";
 import CategoryItem from "../category-item/category-item.component";
+
 import "./directory.styles.scss";
 
-export default function Directory({categories}) {
+export default function Directory({ categories }) {
   return (
-    <div className='categories-container'>
+    <div className="categories-container">
       {categories.map((category) => (
         <CategoryItem category={category} key={category.id} />
       ))}
     </div>
-  )
+  );
 }
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import "./navigation.styles.scss";
+
+export default function Navigation() {
+  return (
+    <>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
+        </Link>
+
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+}
