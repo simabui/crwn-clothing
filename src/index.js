9cb53ec09005e23bbfdb993dd99cc2fb42de5171
simabui <sima_sima@live.ru>
2023-05-15 19:37:36 +0300

    update manifest + icons

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { Elements } from "@stripe/react-stripe-js";
 import { store, persistor } from "./store/store";
 import { stripePromise } from "./utils/stripe/stripe.utils";
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -28,3 +29,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals(console.log);
+serviceWorkerRegistration.register();
