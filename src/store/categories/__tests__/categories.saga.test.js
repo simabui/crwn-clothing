4ef5d0fdca145e86e8ac1533ec0ec24c8a2e91dd
simabui <sima_sima@live.ru>
2023-07-01 11:27:44 +0300

    add fetchCategoriesAsync failure test

--- src/store/categories/__tests__/categories.saga.test.js
+++ src/store/categories/__tests__/categories.saga.test.js
@@ -1,9 +1,10 @@
 import { call } from "typed-redux-saga/macro";
 import { testSaga, expectSaga } from "redux-saga-test-plan";
+import { throwError } from "redux-saga-test-plan/providers";
 
 import { fetchCategoriesAsync, onFetchCategories, categoriesSaga } from "../categories.saga";
 import { CATEGORIES_ACTION_TYPES } from "../category.types";
-import { fetchCategoriesSuccess } from "../category.action";
+import { fetchCategoriesFailed, fetchCategoriesSuccess } from "../category.action";
 import { getCategoriesAndDocuments } from "../../../utils/firebase/firebase";
 
 describe("category saga", () => {
@@ -25,9 +26,18 @@ describe("category saga", () => {
       { id: 2, name: "Category 2" },
     ];
 
-    await expectSaga(fetchCategoriesAsync)
+    return expectSaga(fetchCategoriesAsync)
       .provide([[call(getCategoriesAndDocuments), mockCategoriesArray]])
       .put(fetchCategoriesSuccess(mockCategoriesArray))
+      .run();
+  });
+
+  test("fetchCategoriesAsync failure", async () => {
+    const mockError = new Error("An error occurred");
+
+    return expectSaga(fetchCategoriesAsync)
+      .provide([[call(getCategoriesAndDocuments), throwError(mockError)]])
+      .put(fetchCategoriesFailed(mockError))
       .run();
   });
 });
